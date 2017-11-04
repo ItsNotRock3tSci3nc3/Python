58b4a8722395f54d85e3c3ff4c2cfe189aebb5b8
ItsNotRock3tSci3nc3 <33377709+ItsNotRock3tSci3nc3@users.noreply.github.com>
2017-11-04 11:02:46 -0700

    Create Firstname_Lastname.py

---
+++ Firstname_Lastname.py
@@ -0,0 +1,10 @@
+#/usr/bin/python
+# Program Name: FirstName_LastName
+# Author: Supreet Aradhya
+# Date: November 4, 2017
+firstname = raw_input ("Please enter your first name: ")
+lastname  = raw_input ("Please enter your last name: ")
+print "Hello " + firstname + lastname
+print " Your First name is " + str(len(firstname)) + " Letters Long."
+print " Your Last name is " + str(len(lastname)) + " Letter Long."
+#print " Your First name + Last name is " + str(len(firstname) len(lastname)) + " Letters Long."
